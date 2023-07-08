0a4d3efc9cf48c0e090d4b84809ab50f4b7387bf
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-08 10:42:23 -0400

    fetched random dog image

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+
+import React, {useState, useEffect} from "react"
+
+
+function App() {
+
+   const [dogImage, setDogImage] = useState(null)
+
+  useEffect( ()=> {
+      fetch("https://dog.ceo/api/breeds/image/random")
+      .then(resp => resp.json())
+      .then(data => {setDogImage(data.message)})
+   },[] )
+   
+   if (!dogImage) {
+      return <h2>Loading...</h2>
+   }
+
+
+ console.log(dogImage)
+   return (
+      <div>
+         <h1>App says hi</h1>
+         <img src={dogImage} alt="A Random Dog"></img>
+      </div>
+      
+     
+   )
+}
+
+export default App
